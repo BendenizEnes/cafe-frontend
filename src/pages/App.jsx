49d6c66be7b44d4fb8edf38b2232aba678ced07c
BendenizEnes <e.hacihuseyin03@gmail.com>
2024-07-09 18:15:21 +0300

    File Structure

---
+++ src/pages/App.jsx
@@ -0,0 +1,15 @@
+import '../styles/App.css'
+import {Route, Routes} from "react-router-dom";
+
+function App() {
+
+  return (
+    <Routes>
+      <Route path="/"></Route>
+      <Route path="/dashboard"></Route>
+      <Route path="/calendar"></Route>
+    </Routes>
+  )
+}
+
+export default App
