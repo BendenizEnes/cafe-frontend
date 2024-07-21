245da448ae83ccbf6503748e0008b73bb78c1815
BendenizEnes <e.hacihuseyin03@gmail.com>
2024-07-21 15:19:57 +0300

    Main Page

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -5,11 +5,13 @@ import Security from "./Security.jsx";
 import Settings from "./Settings.jsx";
 import Billing from "@/pages/Billing.jsx";
 import Tables from "@/pages/Tables.jsx";
+import Layout from "@/components/ui/Layout.jsx";
 
 function App() {
 
   return (
     <Routes>
+        <Route path="/" element={<Layout/>}></Route>
         <Route path="/dashboard" element={<Dashboard/>}></Route>
         <Route path="/account" element={<Navigate to="/account/billing"/>}></Route>
         <Route path="/account/settings" element={<Settings/>}></Route>
